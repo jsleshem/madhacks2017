e5781c05c785d1e005f8a13658aa6413fb53f029
Thomas <t.m.kelly@wustl.edu>
2017-02-18 23:19:49 -0600

    time to write this client

--- server/static/js/client.js
+++ server/static/js/client.js
@@ -1,5 +1,5 @@
 $(function() {
-	$("#maze").click(function(event) {
+	/*$("#maze").click(function(event) {
 		// watch out for negative values
 		var params = {
 			x: event.offsetX,
@@ -16,5 +16,39 @@ $(function() {
 			console.log("x: " + params.x);
 			console.log("y: " + params.y);
 		};
+	});*/
+
+	var wheel_position = 0;
+	var centerDisabled = false;
+
+	$("#center").click(function(event) {
+
+		if (centerDisabled)
+			return;
+
+		wheel_position = (wheel_position + 90) % 360;
+
+		var params = {
+			id: "center"
+		};
+
+		$.ajax({
+			url: "move_wall?" + $.param(params),
+			type: "GET"
+		})
+		.done(record);
+
+		function record(){
+			console.log("center wheel succesfully rotated");
+		};
+
+		/*$("#center").animate({
+			"-webkit-transform": 	"rotate("+wheel_position+"deg)",
+			transform:		"rotate("+wheel_position+"deg)"
+		});*/
+
+		centerDisabled = true;
+		setTimeout(function(){centerDisabled = false;}, 3000);
+
 	});
 });
